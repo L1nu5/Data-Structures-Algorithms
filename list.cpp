6e6212c666bb5537e6defa2a40d93b0fd97ee036
L1nu5 <dr.abhi7deore@gmail.com>
2016-07-30 19:19:41 +0530

    Added new features

--- list.cpp
+++ list.cpp
@@ -2,214 +2,307 @@
  * A Basic implementation of Singly Linked List
  * Operations: Insertion, Deletion, Updating, Searching, Reversing & Rotation
  * Challenges: Templating the class, Addition of 2 numbers represented by Linked List
+ * An excercise for solving all the problems related to Linked Lists.
  */
 
+#include <cmath>
 #include <climits>
 #include <iostream>
+#include <algorithm>
+#include <unordered_set>
 
-using namespace std;
+ using namespace std;
 
-class Node
-{
-	int data;
-	Node * next;
-public:
-	Node();
-	Node(int);
+ class Node
+ {
+ 	int data;
+ 	Node * next;
+ public:
+ 	Node();
+ 	Node(int);
 
-	friend class LinkedList;
-};
+ 	friend class LinkedList;
+ };
 
-Node :: Node()
-{
-	data = INT_MAX;
-	next = NULL;
-}
+ Node :: Node()
+ {
+ 	data = INT_MAX;
+ 	next = NULL;
+ }
 
-Node :: Node(int data)
-{
-	this -> data = data;
-	next = NULL;
-}
+ Node :: Node(int data)
+ {
+ 	this -> data = data;
+ 	next = NULL;
+ }
 
-class LinkedList
-{
-	Node * head,* last;
-public:
-	void insertNode(int);
-	void deleteNode(int);
-	bool searchNode(int);
-	void updateNode(int,int);
-	void createList(int);
-	void rotateList(int);
-	void printList();
-	void reverseList();
-};	
+ class LinkedList
+ {
+ 	Node * head,* last;
+ public:
+ 	LinkedList();
+ 	void insertNode(int);
+ 	void deleteNode(int);
+ 	bool searchNode(int);
+ 	void updateNode(int,int);
+ 	void createList(int);
+ 	void rotateList(int);
+ 	void printList();
+ 	void reverseList();
+ 	void removeDups();
+ 	LinkedList addLists(LinkedList);
+ 	int getLength();
 
-void LinkedList :: createList(int data)
-{
-	Node * temp = new Node(data);
-	head = temp;
-	last = head;
-}
+	// More!
+ 	bool isPallindrome();
+ };	
 
-void LinkedList :: insertNode(int data)
-{
-	if(head == NULL){
-		createList(data);
-		return ;
-	}
-		
+ LinkedList :: LinkedList()
+ {
+ 	head = last = NULL;
+ }
 
-	last -> next = new Node(data);
-	last = last -> next;
-}
+ void LinkedList :: createList(int data)
+ {
+ 	Node * temp = new Node(data);
+ 	head = temp;
+ 	last = head;
+ }
 
-void LinkedList :: deleteNode(int data)
-{
-	Node * temp = head;
-	Node * prev;
+ int LinkedList :: getLength()
+ {
+ 	Node * temp = head;
+ 	int size=0;
+ 	while(temp != NULL)
+ 	{
+ 		size ++;
+ 		temp = temp -> next;
+ 	}
+
+ 	return size;
+ }
+
+ void LinkedList :: insertNode(int data)
+ {
+ 	if(head == NULL){
+ 		createList(data);
+ 		return ;
+ 	}
+
+
+ 	last -> next = new Node(data);
+ 	last = last -> next;
+ }
+
+ void LinkedList :: deleteNode(int data)
+ {
+ 	Node * temp = head;
+ 	Node * prev;
 
 	/* Deletion: First Node */
-	if(head -> data == data)
-	{
-		head = head -> next;
-		temp = NULL;
-		delete(temp);
-		return ;
-	}
+ 	if(head -> data == data)
+ 	{
+ 		head = head -> next;
+ 		temp = NULL;
+ 		delete(temp);
+ 		return ;
+ 	}
 
 	/* Deletion: Normal */
-	prev = head;
-	temp = head -> next;
-	while(temp != NULL)
-	{
-		if(temp -> data == data)
-		{
-			prev -> next = temp -> next;
-			if(temp -> next == NULL)
-				last = prev;
+ 	prev = head;
+ 	temp = head -> next;
+ 	while(temp != NULL)
+ 	{
+ 		if(temp -> data == data)
+ 		{
+ 			prev -> next = temp -> next;
+ 			if(temp -> next == NULL)
+ 				last = prev;
 
-			temp -> next = NULL;
-			delete(temp);
-			temp = NULL;
-			return ;
-		}
+ 			temp -> next = NULL;
+ 			delete(temp);
+ 			temp = NULL;
+ 			return ;
+ 		}
 
-		temp = temp -> next;
-		prev = prev -> next;
-	}
-}
+ 		temp = temp -> next;
+ 		prev = prev -> next;
+ 	}
+ }
 
-bool LinkedList :: searchNode(int data)
-{
-	Node * temp = head;
-	int count = 1;
-	bool flag = false;
+ bool LinkedList :: searchNode(int data)
+ {
+ 	Node * temp = head;
+ 	int count = 1;
+ 	bool flag = false;
 
-	while(temp != NULL)
-	{
-		if(temp -> data == data)
-		{
-			cout << "Node found at: " << count << endl;
-			count ++;
-			flag = true;
-		} 
+ 	while(temp != NULL)
+ 	{
+ 		if(temp -> data == data)
+ 		{
+ 			cout << "Node found at: " << count << endl;
+ 			count ++;
+ 			flag = true;
+ 		} 
 
-		temp = temp -> next;
-	}
+ 		temp = temp -> next;
+ 	}
 
-	return flag;
-}
+ 	return flag;
+ }
 
-void LinkedList :: updateNode(int data1,int data2)
-{
-	Node * temp = head;
+ void LinkedList :: updateNode(int data1,int data2)
+ {
+ 	Node * temp = head;
 
-	while(temp != NULL)
-	{
-		if(temp -> data == data1){
-			temp -> data = data2;
-			break;
-		}
-		temp = temp -> next;
-	}
+ 	while(temp != NULL)
+ 	{
+ 		if(temp -> data == data1){
+ 			temp -> data = data2;
+ 			break;
+ 		}
+ 		temp = temp -> next;
+ 	}
 
-	if(temp == NULL)
-		cout << "Node not found!" << endl;
+ 	if(temp == NULL)
+ 		cout << "Node not found!" << endl;
 
-}
+ }
 
-void LinkedList :: rotateList(int count)
-{
-	int size=0;
-	Node *temp = head;
-	Node *h1,*h2;
+ void LinkedList :: rotateList(int count)
+ {
+ 	int size=0;
+ 	Node *temp = head;
+ 	Node *h1,*h2;
 
-	for(;temp!=NULL;temp=temp->next)
-		++size;
+ 	for(;temp!=NULL;temp=temp->next)
+ 		++size;
 
-	temp = head;
-	for(int i=1;i<(size-count);i++)
-		temp = temp -> next;
+ 	temp = head;
+ 	for(int i=1;i<(size-count);i++)
+ 		temp = temp -> next;
 
-	h2 = temp -> next;
-	temp -> next = NULL;
+ 	h2 = temp -> next;
+ 	temp -> next = NULL;
 
-	Node *th = h2;
-	while(th -> next != NULL)
-		th = th -> next;
+ 	Node *th = h2;
+ 	while(th -> next != NULL)
+ 		th = th -> next;
 
-	th -> next = head;
-	head = h2;
-}
+ 	th -> next = head;
+ 	head = h2;
+ }
 
-void LinkedList :: reverseList()
-{
-	Node *curr,*prev,*fwd;
-	prev = NULL;
-	curr = head;
+ void LinkedList :: reverseList()
+ {
+ 	Node *curr,*prev,*fwd;
+ 	prev = NULL;
+ 	curr = head;
 
-	while(curr != NULL)
-	{
-		fwd = curr -> next;
-		curr -> next = prev;
-		prev = curr;
-		curr = fwd;
-	}
+ 	while(curr != NULL)
+ 	{
+ 		fwd = curr -> next;
+ 		curr -> next = prev;
+ 		prev = curr;
+ 		curr = fwd;
+ 	}
 
-	this -> head = prev;
-}
+ 	this -> head = prev;
+ }
 
-void LinkedList :: printList()
-{
-	Node * temp = head;
+ void LinkedList :: printList()
+ {
+ 	Node * temp = head;
 
-	while(temp != NULL){
-		cout << temp -> data << " -> ";
-		temp = temp -> next;
-	}
-	cout << "NULL"<<endl;
-}
+ 	while(temp != NULL){
+ 		cout << temp -> data << " -> ";
+ 		temp = temp -> next;
+ 	}
+ 	cout << "NULL"<<endl;
+ }
 
-int main()
-{
-	LinkedList list;
+ LinkedList LinkedList :: addLists(LinkedList list2)
+ {
+ 	int sum = 0;
+ 	int carry = 0;
 
-	list.createList(11);
-	list.insertNode(2);
-	list.insertNode(10);
-	list.insertNode(76);
-	list.insertNode(5);
-	list.insertNode(13);
-	list.insertNode(44);
-	list.printList();
+ 	int l1 = this -> getLength();
+ 	int l2 = list2.getLength();
+ 	LinkedList sumList;
 
-	list.rotateList(3);
-	list.printList();
+ 	// Check for lengths & handle accordingly
+ 	if(l1 != l2)
+ 	{
+ 		LinkedList temp;
+ 		for(int i=0;i<(abs(l2-l1));i++)
+ 			temp.insertNode(0);
 
-	list.reverseList();
-	list.printList();	
+ 		if(l1 < l2){
+ 			temp.last = this -> head;
+ 			this -> head = temp.head;
+ 		}else if(l1 > l2){
+ 			temp.last = list2.head;
+ 			list2.head = temp.head;
+ 		}
+ 	}
 
-	return 0;
-}
+ 	// Actual Addition of LinkedLists
+ 	Node *t1,*t2;
+ 	t1 = this -> head;
+ 	t2 = list2.head;
+
+ 	while(t1 != NULL)
+ 	{
+ 		sum = carry + t1 -> data + t2 -> data;
+ 		carry = sum / 10;
+ 		sum = sum % 10;
+ 		sumList.insertNode(sum);
+
+ 		t1 = t1 -> next;
+ 		t2 = t2 -> next;
+ 	}
+ 	sumList.insertNode(carry);
+ 	
+ 	return sumList;
+ }
+
+// Function uses O(N) time & O(N) space. Alternative using O(N^2) time & O(1) space
+ void LinkedList :: removeDups()
+ {
+ 	Node *temp = head;
+ 	Node *prev = NULL;
+ 	unordered_set<int> hash;
+
+ 	while(temp != NULL)
+ 	{
+ 		auto search = hash.find(temp -> data);
+ 		if(search != hash.end()){
+ 			prev -> next = temp -> next;
+ 		}else{
+ 			hash.insert(temp -> data);
+ 			prev = temp;
+ 		}
+
+ 		temp = temp -> next;
+ 	}
+ }
+
+ int main()
+ {
+ 	LinkedList listA;
+
+ 	listA.insertNode(1);
+ 	listA.insertNode(1);
+	listA.insertNode(2);
+ 	listA.insertNode(1);
+ 	listA.insertNode(3);
+ 	listA.insertNode(4);
+ 	listA.insertNode(3);
+ 	listA.insertNode(2);
+ 	listA.printList();
+
+ 	listA.removeDups();
+ 	listA.printList();
+
+ 	return 0;
+ }
